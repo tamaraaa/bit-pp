e4c0c8abda1f89f18ab6136aebbdfcfd4561aa34
BIT Student <classroom@bgit.rs>
2018-02-06 17:54:17 +0100

    Funkcije

--- loops/for loop.js
+++ loops/for loop.js
@@ -18,17 +18,46 @@ for (var i = 0; i < 1000; i++) {
 }
 console.log(t);
 */
-var numbers = [1, 1.5, 2, 2.5];
+/*var numbers = [1, 1.5, 2, 2.5];
 var sum = 0;
 var prod = 1;
-if ((numbers[i] * 10) % 10 === 0) {
-    for (var i = 0; i < numbers.length; i++) {
-        /*if ((numbers[i] * 10) % 10 === 0) {*/
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 1 === 0) {
         sum = sum + numbers[i];
         prod = prod * numbers[i];
-
     }
-
 }
+
 console.log(sum);
 console.log(prod);
+*/
+
+function sum(a, b) {
+    var c = a + b;
+    return c;
+}
+var rezultat = sum(2, 3)
+console.log(rezultat);
+
+function checkNum(a) {
+    var x = a % 2 === 0;
+    return x;
+}
+var odd = checkNum(6);
+console.log(odd);
+
+function broj(b) {
+    var h = (b > 99 && b < 1000);
+    return h;
+}
+var cif = broj(874);
+console.log(cif);
+
+function sred(a, b, c, d) {
+    var z = (a + b + c + d) / 4;
+    return z;
+
+}
+
+var unesi = sred(2, 1, 7, 9);
+console.log(unesi);
